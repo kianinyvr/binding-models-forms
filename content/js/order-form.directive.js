4fb7b14ab21bf3691978fcc40cc26e74d14a2a58
kakhavan <kian.akhavan@gmail.com>
2019-02-14 13:13:13 -0800

    added selections to pull-down list with ng-options. Ng-disabled used to show allow submission only when required fields are filled in

--- content/js/order-form.directive.js
+++ content/js/order-form.directive.js
@@ -8,16 +8,20 @@ function orderForm() {
     },
     controller: 'OrderFormController as form',
     template: `
-      <pre>{{ form.data | json}}</pre>
-      <form novalidate ng-submit="form.onSubmit();">
-        <input type="text" ng-model="form.data.name" placeholder="Your name">
-        <input type="email" ng-model="form.data.email" placeholder="Your email">
-        <input type="text" ng-model="form.data.location" placeholder="Your location">
-        <select>
+      <form name="orderForm" novalidate ng-submit="form.onSubmit();">
+        <input name="name" required="" type="text" ng-model="form.data.name" placeholder="Your name">
+        <input name="email" required="" type="email" ng-model="form.data.email" placeholder="Your email">
+        <input name="location" required="" type="text" ng-model="form.data.location" placeholder="Your location">
+        <select
+          name="OrderChoice"
+          required=""
+          ng-model="form.data.product"
+          ng-options="product.label for product in form.products "
+        >
           <option value="">Select...</option>
         </select>
-        <textarea ng-model="form.data.comments" placeholder="Any message (optional)" ></textarea>
-        <button type="submit>
+        <textarea name="comments" ng-model="form.data.comments" placeholder="Any message (optional)" ></textarea>
+        <button type="submit" ng-disabled="orderForm.$invalid">
           Order
         </button>
       </form>
